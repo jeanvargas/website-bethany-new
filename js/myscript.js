804780e4d053713b754bfb87ac2dfa2f53bcb2a0
Jeanmarie Vargas <jeanmarie.vargas@gmail.com>
2016-06-29 23:23:16 -0400

    added jquery.sticky

--- js/myscript.js
+++ js/myscript.js
@@ -3,9 +3,17 @@
 //document.getElementById('pre-loader').style.display='block';
 
 //Pre-loader
-jQuery(window).load(function () {
+/*/jQuery(window).load(function () {
 	//Preloader
 	setTimeout("jQuery('#pre-loader').animate({'opacity' : '0'},300,function(){jQuery('#pre-loader').hide()})",800);
 	setTimeout("jQuery('.page_block').animate({'opacity' : '1'},500)",800);
+	
+	      $("#main-menu").sticky({ topSpacing: 0 });
 
-});
+    });
+
+});*/
+
+jQuery(window).load(function(){
+      $("#main-menu").sticky({ topSpacing: 0 });
+    });
